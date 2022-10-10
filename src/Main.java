d1555d23fa9a35f044db1994762133155fdf4596
Ruslan <gimazevs@mail.ru>
2022-10-11 02:08:41 +0300

    сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,36 @@ public class Main {
             }
             monthOfSavings = monthOfSavings + 1;
         }
+        //задание 5
+        System.out.println(" ");
+        int initialAmount1 = 15000;
+        int percentageOfTheAmount1 = 1;
+        int monthOfSavings1 = 1;
+        int year = 0;
+        while (year <= 9) {
+            percentageOfTheAmount1 = (int) (initialAmount1 * 0.07);
+            initialAmount1 = initialAmount1 + percentageOfTheAmount1;
+            if (monthOfSavings1 % 6 == 0) {
+                System.out.println("Месяц " + monthOfSavings1 + " сумма накоплений " + initialAmount1);
+            }
+            monthOfSavings1 = monthOfSavings1 + 1;
+            year = monthOfSavings1 / 12;
+        }
+        //задание 6
+        System.out.println(" ");
+        int yearCometInThePast = 0;
+        int yearCometInTheFuture = 2122;
+        int yearCometInThePrTime = 2022;
+        for (; yearCometInThePast <= (yearCometInThePrTime + 100); yearCometInThePast = yearCometInThePast + 79) {
+            if (yearCometInThePast >= (yearCometInThePrTime - 200) && yearCometInThePast <= (yearCometInThePrTime + 100)) {
+                System.out.println(yearCometInThePast);
+            }
+        }
+        //задание 7
+        System.out.println(" ");
+        int i3 = 2;
+        for (int e = 1; e <= 10; e++) {
+            System.out.println(e + " * " + i3 + " = " + e * i3);
+        }
     }
 }
